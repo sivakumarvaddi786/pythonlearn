98dd92f040be1c475ed16da63bbe376ab92c82c2
sivakumar vaddi <sivakumarvaddi786@gmail.com>
2022-06-27 11:10:32 +0530

    Update example4.py

--- example4.py
+++ example4.py
@@ -14,3 +14,15 @@ print(x)
 print("Augmented Assignment Operators")
 x += 3
 print(x)
+
+'''
+Operator presedence
+You might have heard about the BODMAS rule in your school’s mathematics class. Python also uses a similar type of rule known as PEMDAS.
+
+P – Parentheses
+E – Exponentiation
+M – Multiplication
+D – Division
+A – Addition
+S – Subtraction
+'''
