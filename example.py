b787b060255b4542e5e6619474fa59a8aef09047
sivakumar vaddi <sivakumarvaddi786@gmail.com>
2022-06-27 10:10:05 +0530

    Create example.py

---
+++ example.py
@@ -0,0 +1,11 @@
+print("This is sivakumar")
+print('----------' * 20 )
+print('what\'s up? ')
+print("The value for 2 * 2 is: ", 2*2)
+number1 = 10
+number2 = 20
+print("Number is ", number1 *  number2)
+
+P_Name = Siva 
+P_Age  =  33
+P_New  = True
