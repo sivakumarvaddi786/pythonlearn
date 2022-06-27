94621688e4eb541fa93a3d29a3257ed73962fe60
sivakumar vaddi <sivakumarvaddi786@gmail.com>
2022-06-27 10:51:07 +0530

    Create example3.py

---
+++ example3.py
@@ -0,0 +1,13 @@
+'''
+Difference between method and function
+Simply, function and method both look similar as they perform in almost similar way, but the key difference is the concept of ‘Class and its Object‘.
+
+Functions can be called only by its name, as it is defined independently.
+
+But methods can’t be called by its name only, we need to invoke the class by a reference of that class in which it is defined, 
+i.e. method is defined within a class and hence they are dependent on that class.
+
+String1 = "This is my first line of code"
+print(len(String1)) This is Function
+print(String1.upper()) This is Method
+'''
