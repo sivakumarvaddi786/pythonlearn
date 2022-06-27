8783353ff88f6dbc1477467887dcefe504cd019c
sivakumar vaddi <sivakumarvaddi786@gmail.com>
2022-06-27 10:55:54 +0530

    Update example3.py

--- example3.py
+++ example3.py
@@ -1,5 +1,9 @@
 '''
 Difference between method and function
+
+Methods are associated with the objects of the class they belong to. Functions are not associated with any object.
+We can invoke a function just by its name. Functions operate on the data you pass to them as arguments.
+
 Simply, function and method both look similar as they perform in almost similar way, but the key difference is the concept of ‘Class and its Object‘.
 
 Functions can be called only by its name, as it is defined independently.
